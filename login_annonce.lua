5cf21a85b5d6954a58e6854906ed3dc6b07ea232
Your Name <you@example.com>
2021-04-16 09:48:01 +0200

    retrait com

--- login_annonce.lua
+++ login_annonce.lua
@@ -3,5 +3,5 @@ local PLAYER_EVENT_ON_LOGIN = 3
 local function WorldAnnonce(event, player)
     SendWorldMessage(player:GetName().."vient de se connecter")
 end
--- testgithub webhook 2
+
 RegisterPlayerEvent(PLAYER_EVENT_ON_LOGIN, WorldAnnonce)
