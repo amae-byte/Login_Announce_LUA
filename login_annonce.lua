b037ad3a9647feb8237e201ed15a7bd7db66bd02
Your Name <you@example.com>
2021-04-16 10:52:07 +0200

    ajout players count

--- login_annonce.lua
+++ login_annonce.lua
@@ -2,6 +2,7 @@ local PLAYER_EVENT_ON_LOGIN = 3
 
 local function WorldAnnonce(event, player)
     SendWorldMessage(player:GetName().." vient de se connecter")
+    SendWorldMessage(GetPlayerCount().." joueur(s) actuellement en ligne") 
 end
 
 RegisterPlayerEvent(PLAYER_EVENT_ON_LOGIN, WorldAnnonce)
