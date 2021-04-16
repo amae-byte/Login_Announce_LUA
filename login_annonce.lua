da4f06ba04a3dbdc4d80a7141045cfd023c6f4f8
Your Name <you@example.com>
2021-04-16 11:35:23 +0200

    end function

--- login_annonce.lua
+++ login_annonce.lua
@@ -2,12 +2,14 @@ local PLAYER_EVENT_ON_LOGIN = 3
 
 local function WorldAnnonce(event, player)
     SendWorldMessage(player:GetName().." vient de se connecter")
-    SendWorldMessage(GetPlayerCount().." joueur(s) actuellement en ligne")
+    
     local total_world_players = GetPlayerCount()
+
     if(total_world_players <= 1) then
         SendWorldMessage(total_world_players.." joueur actuellement en ligne")
     else 
         SendWorldMessage(total_world_players.." joueurs actuellement en ligne")
     end
+end
 
 RegisterPlayerEvent(PLAYER_EVENT_ON_LOGIN, WorldAnnonce)
