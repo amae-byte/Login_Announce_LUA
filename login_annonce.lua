c5fd51976854834c4d33d53c677d04f095a0b502
Your Name <you@example.com>
2021-04-16 09:34:28 +0200

    nom du player

--- login_annonce.lua
+++ login_annonce.lua
@@ -1,8 +1,7 @@
 local PLAYER_EVENT_ON_LOGIN = 3
 
 local function WorldAnnonce(event, player)
-    SendWorldMessage("player logged")
-   
+    SendWorldMessage(player:GetName().."vient de se connecter")
 end
 
 RegisterPlayerEvent(PLAYER_EVENT_ON_LOGIN, WorldAnnonce)
