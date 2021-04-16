3dbf7f85742b5d1f1334635fdbff57cd7cb03c1d
Your Name <you@example.com>
2021-04-16 11:21:18 +0200

    annonce correction

--- login_annonce.lua
+++ login_annonce.lua
@@ -2,7 +2,12 @@ local PLAYER_EVENT_ON_LOGIN = 3
 
 local function WorldAnnonce(event, player)
     SendWorldMessage(player:GetName().." vient de se connecter")
-    SendWorldMessage(GetPlayerCount().." joueur(s) actuellement en ligne") 
-end
+    SendWorldMessage(GetPlayerCount().." joueur(s) actuellement en ligne")
+    total_world_players = GetPlayerCount()
+    if(GetPlayerCount() =< 1) then
+        SendWorldMessage(total_world_players.." joueur actuellement en ligne")
+    else 
+        SendWorldMessage(total_world_players.." joueurs actuellement en ligne")
+    end
 
 RegisterPlayerEvent(PLAYER_EVENT_ON_LOGIN, WorldAnnonce)
